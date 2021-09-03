9cbc64c911fd9e5fdbc805f014f3befe12e13699
Doug <karaballo.douglas@hotmail.com>
2021-09-03 14:59:01 -0400

    Creada la pagina de exito al registrar un usuario

---
+++ vistas/modulos/exito.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+    if(isset($_SESSION["super"])){
+?>
+    <div class="content-exit">
+        <h2>El usuario fue registrado con éxito</h2>
+        <p>Desea volver?</p>
+        <a class="content-exit__button" href="index.php?ruta=productos">Productos</a>
+    </div>
+<?php
+    }else if(isset($_SESSION["admin"])|| isset($_SESSION["user"])){
+        header("location:index.php?ruta=productos");
+    }else{
+        header("location:index.php?ruta=ingreso");
+        exit();
+    }
+?>
