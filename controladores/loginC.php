92624bdd0fb95e69291c63f05e10f877c91db515
Doug <karaballo.douglas@hotmail.com>
2021-09-16 08:53:33 -0400

    Modificaciones y correccion de errores de un ingreso con usuario y contraseña que no existen

--- controladores/loginC.php
+++ controladores/loginC.php
@@ -14,25 +14,25 @@ class LoginC{
             $tablaBD = "usuario";
 
             $respuesta = LoginM:: IngresoM($datosc, $tablaBD);
-
-            if($respuesta["usuario"]==$_POST["usuarioI"] && $respuesta["clave"]==$_POST["claveI"]){
-                session_start();
-                if($respuesta["id_permiso"]=="1"){
-                    $_SESSION["super"]=true;
-                    $_SESSION["user_id"]=$respuesta["id"];
-                    header("location:index.php?ruta=productos");
-                }else if ($respuesta["id_permiso"]=="2"){
-                    $_SESSION["admin"]=true;
-                    $_SESSION["user_id"]=$respuesta["id"];
-                    header("location:index.php?ruta=productos");
-                }else{
-                    $_SESSION["user"]=true;
-                    $_SESSION["user_id"]=$respuesta["id"];
-                    header("location:index.php?ruta=productos");
-                }
-
+            if (empty($respuesta)){
+                echo "Usuario o contraseña erroneos";
+            }else if($respuesta["usuario"]==$_POST["usuarioI"] && $respuesta["clave"]==$_POST["claveI"]){
+                    session_start();
+                    if($respuesta["id_permiso"]=="1"){
+                        $_SESSION["super"]=true;
+                        $_SESSION["user_id"]=$respuesta["id"];
+                        header("location:index.php?ruta=productos");
+                    }else if ($respuesta["id_permiso"]=="2"){
+                        $_SESSION["admin"]=true;
+                        $_SESSION["user_id"]=$respuesta["id"];
+                        header("location:index.php?ruta=productos");
+                    }else{
+                        $_SESSION["user"]=true;
+                        $_SESSION["user_id"]=$respuesta["id"];
+                        header("location:index.php?ruta=productos");
+                    }
             }else{
-                echo "ERROR AL INGRESAR";
+                echo "Usuario o contraseña erroneos";
             }
         }
     }
